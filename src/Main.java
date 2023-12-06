f903dbdc3fbc55c772b056b32c09082845700f2b
floralgreen <carducci.federico20@gmail.com>
2023-12-06 22:50:04 +0100

    Creata testata Menù e nome ristorante

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String nomeRistorante = "Ristorante Team2 Java20";
+
+        System.out.println(nomeRistorante + "\n" +
+                "Menù di Carne: ");
+
     }
 
 }

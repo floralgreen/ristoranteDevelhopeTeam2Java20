06a2b293b9efd7532f72919746eabf5d1992ecef
floralgreen <carducci.federico20@gmail.com>
2023-12-06 22:55:31 +0100

    Creato menù dolci

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,12 @@ public class Main {
 
         System.out.println(nomeRistorante + "\n" +
                 "Menù di Carne: ");
-
+        System.out.println("Dolci: \n" +
+                "- Tiramisù\n" +
+                "- Panna Cotta\n" +
+                "- Sorbetto Limone\n" +
+                "- Crostata c/marmellata di visciole\n" +
+                "- Brownie al cioccolato");
     }
 
 }

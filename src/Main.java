a7cc92a3242f9bc1f6fb72b323bc02610f8cf891
Ivan <ivanpiccioni.work@gmail.com>
2023-12-13 16:15:01 +0100

    aggiunto array SecondoPiatto nel main e riepito

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
+         SecondoPiatto[] secondiPiatti=new SecondoPiatto[5];
+
 
         SecondoPiatto secondoPiatto1 = new SecondoPiatto("Salti in bocca alla romana",8.50,"Fettine di vittella,salvia,vino bianco,farina 00,prosciutto crudo,burro,pepe,olio");
         SecondoPiatto secondoPiatto2 = new SecondoPiatto("Tagliata di manzo",18.50,"Controfiletto di manzo,sale,olio,rosmarino,pepe");
         SecondoPiatto secondoPiatto3 = new SecondoPiatto("Grigliata mista",23.0,"4 salsicce di maiale,4 pancetta di maiale,4 costolette di agnello ,4 braciole di maiale,pepe,olio,aglio,rosmarino,aceto di vino");
         SecondoPiatto secondoPiatto4 = new SecondoPiatto("Abbacchio fritto",12.0,"costolette d agnello,sale,pepe,olio,uovo,pangrattato");
         SecondoPiatto secondoPiatto5 = new SecondoPiatto("Arrosticini alla brace",11.0,"arrosticini di pecora,sale");
+
+        secondiPiatti[0]=secondoPiatto1;
+        secondiPiatti[1]=secondoPiatto2;
+        secondiPiatti[2]=secondoPiatto3;
+        secondiPiatti[3]=secondoPiatto4;
+        secondiPiatti[4]=secondoPiatto5;
 
 
 

81db69d01b4f3a447e69f2ce43b38c3c0e3ba843
Domenico <domenicoribaudo06@gmail.com>
2023-12-23 12:08:01 +0100

    Creata classe antipasto

---
+++ src/Antipasto.java
@@ -0,0 +1,23 @@
+public class Antipasto extends Portata {
+    private String ingredienti;
+
+    public Antipasto(CurrencyEnum currencyEnum, String nomePortata, double prezzoPortata, String ingredienti) {
+        super(currencyEnum, nomePortata, prezzoPortata);
+        this.ingredienti = ingredienti;
+
+    }
+
+    public String getIngredienti() {
+        return ingredienti;
+    }
+
+    public void setIngredienti(String ingredienti) {
+        this.ingredienti = ingredienti;
+    }
+
+    @Override
+    public void stampaInfoPortata() {
+        super.stampaInfoPortata();
+        System.out.println(ingredienti);
+    }
+}

ad788b17fdcbb4174964794f2585ea9db41f31a0
floralgreen <carducci.federico20@gmail.com>
2023-12-23 07:52:32 +0100

    Creata classe Ristorante

---
+++ src/Ristorante.java
@@ -0,0 +1,52 @@
+import java.util.HashSet;
+
+public class Ristorante {
+
+    private HashSet<Menu> menuDisponibili;
+    private String nomeRistorante;
+
+    public Ristorante(String nomeRistorante){
+        this.menuDisponibili = new HashSet<Menu>();
+        this.nomeRistorante = nomeRistorante;
+    }
+
+    public HashSet<Menu> getMenuDisponibili() {
+        return menuDisponibili;
+    }
+
+    public void setMenuDisponibili(HashSet<Menu> menuDisponibili) {
+        this.menuDisponibili = menuDisponibili;
+    }
+
+    public String getNomeRistorante() {
+        return nomeRistorante;
+    }
+
+    public void setNomeRistorante(String nomeRistorante) {
+        this.nomeRistorante = nomeRistorante;
+    }
+
+    public void aggiungiMenuAlRistorante(Menu menuDaAggiungere){
+        if (menuDaAggiungere != null) {
+            menuDisponibili.add(menuDaAggiungere);
+        } else {
+            System.out.println("Impossibile aggiungere il menu alla lista");
+        }
+    }
+
+    public void rimuoviMenuDalRistorante(Menu menuDaRimuovere) {
+        if (menuDisponibili.contains(menuDaRimuovere)) {
+            menuDisponibili.remove(menuDaRimuovere);
+        } else {
+            System.out.println("Impossibile rimuovere il menu selezionato, non esistente");
+        }
+    }
+    public void stampaMenuDisponibili(){
+
+        for (Menu menu: menuDisponibili) {
+            menu.stampaMenu();
+            System.out.println();
+        }
+
+    }
+}

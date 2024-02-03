1f3f3b6c4ea2d6c98d50510dc3588b6e67d8d63f
floralgreen <carducci.federico20@gmail.com>
2024-02-03 12:26:23 +0100

    create tabelle prenotazioni e tavoli

--- db_ristorante.sql
+++ db_ristorante.sql
@@ -1,1 +1,24 @@
 CREATE DATABASE IF NOT EXISTS ristorante_develhope;
+
+#Federico Carducci 03/02/2024 | creazione tabella prenotazioni
+CREATE TABLE IF NOT EXISTS ristorante_develhope.prenotazioni
+(
+    id_prenotazione INT primary key auto_increment not null,
+    tavolo_id       int                            not null,
+    customer_id     int                            not null
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+#Federico Carducci 03/02/2024 | creazione tabella tavoli
+CREATE TABLE IF NOT EXISTS ristorante_develhope.tavoli(
+    id_tavolo int primary key auto_increment not null, #id usato anche come num_tavolo
+    isFree    BOOL not null default true,
+    posti_tavolo int not null,
+    posti_occupati int,
+    ristorante_id int not null
+)
+ENGINE = InnoDB
+DEFAULT CHARSET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;

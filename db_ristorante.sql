9e88ee75d63f4b2bca96bb50bb1d21f34a2e9558
Domenico <domenicoribaudo06@gmail.com>
2024-02-03 16:12:00 +0100

    Create tabelle customers e ristoranti

--- db_ristorante.sql
+++ db_ristorante.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS ristorante_develhope;
 
+
 #Federico Carducci 03/02/2024 | creazione tabella prenotazioni
 CREATE TABLE IF NOT EXISTS ristorante_develhope.prenotazioni
 (
@@ -22,3 +23,24 @@ CREATE TABLE IF NOT EXISTS ristorante_develhope.tavoli(
 ENGINE = InnoDB
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
+
+#Domenico Ribaudo 03/02/2024 | creazione tabella customers 
+CREATE TABLE IF NOT EXISTS ristorante_develhope.customers(
+    id_customer int primary key auto_increment not null, 
+    first_name  varchar(255) not null,
+    last_name  varchar(255) not null,
+    age        int not null,
+    menu ENUM('carne', 'pesce', 'vegano','nonpreference'),
+    gender ENUM('male', 'female', 'unspecified') 
+);
+
+#Domenico Ribaudo 03/02/2024 | creazione tabella ristoranti
+CREATE TABLE IF NOT EXISTS ristorante_develhope.ristoranti(
+    id_ristorante int primary key auto_increment not null, 
+    nome_ristorante  varchar(255) not null,
+    tot_tavoli_ristorante int not null,
+    prenotazione_id int not null
+);
+
+
+

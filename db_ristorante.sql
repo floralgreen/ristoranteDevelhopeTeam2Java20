06f88be66846ef1145233c44ab8620cbc83b1514
Ivan <ivanpiccioni.work@gmail.com>
2024-02-06 10:33:10 +0100

    modificata tabella portate

--- db_ristorante.sql
+++ db_ristorante.sql
@@ -41,13 +41,13 @@ CREATE TABLE IF NOT EXISTS ristorante_develhope.ristoranti(
     tot_tavoli_ristorante int not null,
     prenotazione_id int not null
 );
-# ivan piccioni 05/02/2024 | creazione portate
-CREATE TABLE Portate (
-    id_portata INT PRIMARY KEY,
-    nome_portata VARCHAR(255),
-    prezzo_portata VARCHAR(255),
-    tipo_portata_enum Enum,  
-    currency_enum Enum       
+# ivan piccioni 05/02/2024 | creazione portata
+CREATE TABLE IF NOT EXISTS ristorante_develhope.portate(
+    id_portata INT PRIMARY key auto_increment not null,
+    nome_portata VARCHAR(255) not null,
+    prezzo_portata VARCHAR(255) not null,
+    tipo_portata_enum Enum('antipasto','primopiatto','secondopiatto','dolce','bevanda')not null,
+    currency_enum Enum ('euro','us_dollar','pound_sterling')not null      
 );
 
 

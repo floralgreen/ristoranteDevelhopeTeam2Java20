43092e77da647489d5724bd1d8e227d743404198
floralgreen <carducci.federico20@gmail.com>
2024-02-10 11:25:14 +0100

    creata tabella dolci

--- db_ristorante.sql
+++ db_ristorante.sql
@@ -26,17 +26,17 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 #Domenico Ribaudo 03/02/2024 | creazione tabella customers 
 CREATE TABLE IF NOT EXISTS ristorante_develhope.customers(
-    id_customer int primary key auto_increment not null, 
+    id_customer int primary key auto_increment not null,
     first_name  varchar(255) not null,
     last_name  varchar(255) not null,
     age        int not null,
     menu ENUM('carne', 'pesce', 'vegano','nonpreference'),
-    gender ENUM('male', 'female', 'unspecified') 
+    gender ENUM('male', 'female', 'unspecified')
 );
 
 #Domenico Ribaudo 03/02/2024 | creazione tabella ristoranti
 CREATE TABLE IF NOT EXISTS ristorante_develhope.ristoranti(
-    id_ristorante int primary key auto_increment not null, 
+    id_ristorante int primary key auto_increment not null,
     nome_ristorante  varchar(255) not null,
     tot_tavoli_ristorante int not null,
     prenotazione_id int not null
@@ -49,6 +49,21 @@ nome_menu varchar(255) not null,
 nome_cuoco varchar(255) not null,
 tipo_menu enum('carne','pesce','vegano','nonpreference')not null
 );
+
+#Federico Carducci 10/02/2024 | creazione tabella dolci
+create table if not exists dolci (
+    id_dolce             int auto_increment,
+    nome_dolce           varchar(255)                                                           not null,
+    prezzo_dolce         DOUBLE                                                                 not null,
+    tipo_portata_enum    enum ('antipasto', 'primopiatto', 'secondopiatto', 'bevanda', 'dolce') not null,
+    currency_enum        enum ('euro', 'us_dollar', 'pound_sterling')                           not null,
+    ingredienti          varchar(500)                                                           not null,
+    preparato_al_momento bool                                                                   not null,
+    menu_id              int                                                                    not null,
+    constraint dolci_pk primary key (id_dolce)
+);
+
+
 
 
 
